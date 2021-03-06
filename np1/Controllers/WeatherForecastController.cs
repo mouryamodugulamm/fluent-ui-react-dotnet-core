7f61b4a26f5f8826b94a51e3e4b5873627031202
Mourya <mouryamodugulamm@gmail.com>
2021-03-06 21:23:55 +0530

    new branch

--- np1/Controllers/WeatherForecastController.cs
+++ np1/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace np1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezingb", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
