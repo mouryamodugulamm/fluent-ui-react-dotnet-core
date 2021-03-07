908eb3f990be574a731d25421a42a8b31141593d
Mourya <mouryamodugulamm@gmail.com>
2021-03-07 10:54:51 +0530

    Card added

---
+++ np1/ClientApp/src/components/Card/Card.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Persona,
+  Text,
+  Image,
+  Stack,
+  Icon,
+  PersonaSize,
+  IconButton,
+} from "@fluentui/react";
+import CardStyles, { CardTokens } from "./styles/Card.Styles";
+import { Link } from "react-router-dom";
+
+const Card = (props) => {
+  return (
+    <Stack className={CardStyles.maxWidth} tokens={CardTokens.cardStak}>
+      <Link to={`/user-profile/${props.data.userid}`}>
+        <Persona
+          text={props.data.fullName}
+          secondaryText="Feb 2, 2019"
+          size={PersonaSize.size24}
+          imageUrl="https://placehold.it/24X24"
+        />
+      </Link>
+      <Stack.Item>
+        <Image src="https://placehold.it/286x144" width="100%" />
+      </Stack.Item>
+      <Text className={CardStyles.siteText}>{props.data.title}</Text>
+      <Text className={CardStyles.descriptionText}>{props.data.comment}</Text>
+      <Text className={CardStyles.helpfulText}>
+        Is this recommendation helpful?
+      </Text>
+
+      <Stack.Item>
+        <Stack
+          horizontal
+          tokens={CardTokens.cardFooterStack}
+          padding="12px 0 0"
+          styles={{ root: { borderTop: "1px solid #F3F2F1" } }}
+        >
+          <IconButton
+            iconProps={{ iconName: "RedEye" }}
+            className={CardStyles.icon}
+          />
+          <IconButton
+            iconProps={{ iconName: "SingleBookmark" }}
+            className={CardStyles.icon}
+          />
+          <Stack.Item grow={1}>
+            <span />
+          </Stack.Item>
+
+          <IconButton
+            iconProps={{ iconName: "MoreVertical" }}
+            className={CardStyles.icon}
+          />
+        </Stack>
+      </Stack.Item>
+    </Stack>
+  );
+};
+
+export default Card;
--- np1/ClientApp/src/components/pages/Dashboard/Dashboard.jsx
+++ np1/ClientApp/src/components/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import Card from "../../Card";
+
+import fakeData from "./__mocks__/MOCK_DATA.json";
 
 export default function Dashboard() {
   return (
     <div>
       <h3>Dashboard Page</h3>
+
+      {fakeData.map((row) => {
+        return <Card data={row} key={row.userid} />;
+      })}
     </div>
   );
 }
